3ff73ae56d9f166af32d89cdc9548f312dc77af0
lucerohospina <lucerojhp@gmail.com>
2018-02-21 05:06:57 -0500

    agrega funcionalidad de input search y README

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # FORO COOL
 
-### Uso de Api para crear un foro de opinión.
+Uso de Api para crear un foro de opinión.
+
+![main-view](https://user-images.githubusercontent.com/32287019/36474033-4ba96766-16c4-11e8-8920-23ce4599fa3b.JPG)
+
+Se desarrolló un foro de opinión con las siguientes funcionalidades:
+
+* El usuario al cargar la página visualiza todos los temas disponibles del foro.
+
+* El usuario puede crear un tema mediante el boton de 'crear tema'  
+
+* El usuario puede filtrar mediante el input; este tomo en consideración a todo el contenido del espacio que contiene cada tema: titulo de tema, autor y numero de respuestas.
+
+#### Desarrollado por: Lucero Hospina y Milagros Palma.
---
+++ public/js/functions.js
@@ -0,0 +1,109 @@
+/**
+ * @param String name
+ * @return String
+ */
+// function getParameterByName(name) {
+//     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+//     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+//     results = regex.exec(location.search);
+//     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+// }
+
+var $topic = $('.topic');
+var $auhtor = $('.author');
+var $responses = $('.response-count');
+var $createTopicBtn = $('#create-btn');
+
+
+$.ajax({
+    url: 'http://examen-laboratoria-sprint-5.herokuapp.com/topics'
+}).done(handleSuccess)
+.fail(handleError);
+
+function handleSuccess(data) {
+    // console.log(data);
+    // console.log(data.length);
+        var jumbotronContent = '';
+        for(i=0; i<data.length; i++) {
+        var topicTitle = data[i].content;
+        var authorUser = data[i].author_name;
+        var responsesCount = data[i].responses_count;
+        // console.log(data[i]);
+        jumbotronContent += `
+        <div class="card mb-3">
+        <div class="card-body">
+          <p class="card-title"><strong>TEMA</strong>: ${topicTitle}</p>
+          <p class="card-text"><strong>por</strong>: ${authorUser}</p>
+          <hr class="my-4">
+        <p class="card-text"><strong>Respuestas</strong>: ${responsesCount}</p>
+        </div>
+        </div>
+        `
+    };
+    $topic.html(jumbotronContent);
+}
+
+function handleError() {
+    consol.log('something went wrong');
+}
+
+$createTopicBtn.click(function(event){
+    // event.preventDefault();
+    var $inputAuthorName = $('#author-name');
+    var $inputNewTopic = $('#new-topic');
+    
+    var body = {
+        author_name: $inputAuthorName.val(),
+        content: $inputNewTopic.val()
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://examen-laboratoria-sprint-5.herokuapp.com/topics',
+        data: body,
+        success: addingEntrance
+    })
+
+    function addingEntrance(newTopic) {
+        console.log(JSON.stringify(newTopic));
+        console.log(newTopic);
+        console.log(newTopic.author_name);
+        console.log(newTopic.content);
+        // $(document).attr('location').href='index.html'
+        // alert('Tema creado');
+        // $('#form-modal').modal('hide');
+        
+        // var bodyString = JSON.stringify(body);
+        // console.log(bodyString.author_name);
+        // console.log(bodyString.name);
+        // console.log(newData.name);
+    }
+})
+
+
+$('#topic-search').keyup(function(){
+    console.log($(this).val());
+    var topicSearched = $(this).val();
+    $('.card').hide();
+    $('.card').each(function() {
+        console.log($(this).text())
+        var savingData = $(this).text();
+        if (savingData.indexOf(topicSearched) > -1) {
+            $(this).show();
+        }
+    })
+})
+
+// var request = new XMLHttpRequest();
+
+// request.open('GET', 'http://examen-laboratoria-sprint-5.herokuapp.com/topics/763/responses');
+
+// request.onreadystatechange = function () {
+//   if (this.readyState === 4) {
+//     console.log('Status:', this.status);
+//     console.log('Headers:', this.getAllResponseHeaders());
+//     console.log('Body:', this.responseText);
+//   }
+// };
+
+// request.send();
